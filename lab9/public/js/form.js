8e2a627bbba4a508260b4a615a5e5753686b5ccd
cullinap <cullinap@gmail.com>
2022-04-01 23:40:18 -0400

    did some error checking

--- lab9/public/js/form.js
+++ lab9/public/js/form.js
@@ -1,36 +1,47 @@
 // alert('connected')
 
+
 function checkPrimeNumber(number){
-    //if num == 1 or 0 
+    if(number == 1 || number == 0)
+        return false
+    
     for(let i = 2, s = Math.sqrt(number); i <= s; ++i)
         if(number % i === 0) return false
     return true
 }
 
-function elementPlacer(value, msg) {
-    let ul = document.getElementById('list');
+function colorMeElement(ele, isPrime){
+    if(isPrime)
+        return ele.classList.add('is-prime')
+    return ele.classList.add('not-prime')
+}
+
+function elementPlacer(value, msg, isPrime) {
+    let ul = document.getElementById('attempts');
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(value + msg));
+    colorMeElement(li, isPrime)
     ul.appendChild(li);
+}
+
+function isInt(value) {
+    let x = parseFloat(value);
+    return !isNaN(value) && (x | 0) === x;
 }
 
 function main(){
     let myValue = document.getElementById('myform').value;
-    if (isNaN(myValue)) {
+
+    if (isNaN(myValue) || !isInt(myValue)) {
         alert('you must enter an integer')
         return
     }
 
     if (checkPrimeNumber(myValue)) {
-        elementPlacer(myValue, ' is a prime number')
+        elementPlacer(myValue, ' is a prime number', true)
     } else {
-        elementPlacer(myValue, ' is not a prime number')
+        elementPlacer(myValue, ' is not a prime number', false)
     }
 }
 
-
-
-// document.querySelector('#myform').addEventListener('input', (event)=>{
-//     console.log(event.target.value)
-// })
 
