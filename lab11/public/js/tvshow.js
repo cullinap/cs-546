67d95bd7a3d05a3ff64b5715f3b25ad5db447406
cullinap <cullinap@gmail.com>
2022-04-19 19:57:00 -0400

    function 2

--- lab11/public/js/tvshow.js
+++ lab11/public/js/tvshow.js
@@ -1,18 +1,37 @@
+$(document).ready(function () {
+  let allShowsUrl = "http://api.tvmaze.com/shows";
+  let invShowUrl = "http://api.tvmaze.com/search/shows?q=";
 
+  $.get(allShowsUrl, function (showname) {
+    // alert(showname[0].name)
+    // alert(showname[0]._links.self.href)
+    for (let i = 0; i <= showname.length; ++i) {
+      $("#showList")
+        .append(
+          `<li><a href="${showname[i]._links.self.href}">${showname[i].name}</a></li>`
+        )
+        .show();
+    }
+  });
 
-$( document ).ready(function() {
-    let url = 'http://api.tvmaze.com/shows'
-    
-    $.get(url, function(showname){
-        for(let i=0; i<4; ++i) {
-            $("#showList").append('<li>an item</li>').show()
+  $("#searchForm").submit(function (event) {
+    event.preventDefault();
+
+    let searchTerm = $("#search_term").val();
+    alert(searchTerm)
+    $("#showList").empty();
+
+    $.get(
+      `http://api.tvmaze.com/search/shows?q=${searchTerm}`,
+      function (showname) {
+        for (show of showname) {
+          $("#showList").append(`<li>${show.show.name}</li>`);
         }
-    })
+      }
+    );
+  });
 
 
-    
 });
-
-
 
 //https://stackoverflow.com/questions/11240915/how-to-i-append-lis-to-a-current-ul-using-load
