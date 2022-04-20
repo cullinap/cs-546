701acb366a4f8e04b6b3663f826a2889dde3d016
cullinap <cullinap@gmail.com>
2022-04-19 22:56:57 -0400

    change of plans

--- lab11/public/js/tvshow.js
+++ lab11/public/js/tvshow.js
@@ -1,35 +1,70 @@
+
+
 $(document).ready(function () {
   let allShowsUrl = "http://api.tvmaze.com/shows";
   let invShowUrl = "http://api.tvmaze.com/search/shows?q=";
 
+  function link (showname) {
+    $(showname).click(function(event){
+        event.preventDefault()
+        $("#show").empty()
+
+        let target = event.target.href
+        $.get(target, function() {
+            $("#show").append(`<h1>hey</h1>`)
+        })
+
+        $("#showList").hide()
+        $("show").show()
+
+    })
+  }
+
   $.get(allShowsUrl, function (showname) {
     // alert(showname[0].name)
     // alert(showname[0]._links.self.href)
-    for (let i = 0; i <= showname.length; ++i) {
-      $("#showList")
+
+    for(show in showname) {
+        console.log(showname[show])
+        $("#showList")
         .append(
-          `<li><a href="${showname[i]._links.self.href}">${showname[i].name}</a></li>`
+          `<li><a href="${showname[show]._links.self.href}">${showname[show].name}</a></li>`
         )
-        .show();
     }
+
+    $("#showList").show()
+
+    // for (let i = 0; i <= showname.length; ++i) {
+    //   $("#showList")
+    //     .append(
+    //       `<li><a href="${showname[i]._links.self.href}">${showname[i].name}</a></li>`
+    //     ).show()
+    // }
+
+   
   });
 
   $("#searchForm").submit(function (event) {
     event.preventDefault();
 
-    let searchTerm = $("#search_term").val();
-    alert(searchTerm)
+    let searchTerm = $("#search_term").val().trim();
+    if (searchTerm.length == 0) {
+      $("#error").append(`<p>Try another search term</p>`).show();
+    }
+
     $("#showList").empty();
 
-    $.get(
-      `http://api.tvmaze.com/search/shows?q=${searchTerm}`,
-      function (showname) {
-        for (show of showname) {
-          $("#showList").append(`<li>${show.show.name}</li>`);
-        }
+    $.get(invShowUrl + searchTerm, function (showname) {
+      for (let i = 0; i <= showname.length; ++i) {
+        $("#showList").append(
+          `<li><a href="${showname[i].show._links.self.href}">${showname[i].show.name}</a></li>`
+        );
       }
-    );
+
+
+    });
   });
+
 
 
 });
